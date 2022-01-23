bfa3f2d0a3b4379f49af26961c8ac87ecff2a629
thales <jiangtai20@mails.ucas.ac.cn>
2022-01-23 22:35:54 +0800

    combine app

---
+++ nsfw/precache-manifest.be43de149185d19bb4e1aa69c1b1d45d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "97a7051616ae9c3c1c50",
+    "url": "./static/css/main.781078ba.chunk.css"
+  },
+  {
+    "revision": "97a7051616ae9c3c1c50",
+    "url": "./static/js/main.97a70516.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "df98ac8182301db38936",
+    "url": "./static/css/2.6cb907dc.chunk.css"
+  },
+  {
+    "revision": "df98ac8182301db38936",
+    "url": "./static/js/2.df98ac81.chunk.js"
+  },
+  {
+    "revision": "bc2feedafba03279f721a49115c7069a",
+    "url": "./static/media/rob.bc2feeda.jpg"
+  },
+  {
+    "revision": "1bf263d72db59e55546d0a380c1257a1",
+    "url": "./static/media/logo.1bf263d7.jpeg"
+  },
+  {
+    "revision": "5496233cd1ce8b5525cd13fe8b365080",
+    "url": "./index.html"
+  }
+];
